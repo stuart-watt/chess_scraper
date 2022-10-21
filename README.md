54b7e873a29799483338ea26b5b603f60047cb97
Stuart Watt <40311652+stuart-watt@users.noreply.github.com>
2022-10-21 16:54:05 +0800

    Some new changes after a long break (#2)
    * Updated the readme to get started
    
    * move scraper into scripts
    
    * update readme
    
    * update click commands
    
    * get notebook working again
    
    * delete scripts
    
    * check for incorrect username

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # chess_scraper
 A scraper to pull data from chess.com and run some analysis.
 
-TODO
+### First steps
+
+1. Install make
+
+run `sudo apt-get install build-essential` in the terminal. This will install uild-essential and allow you to run commands defined in the Makefile.
+
+2. Install miniconda int your home directory
+
+This code will operate out of a conda environment. 
+
+First download Miniconda to your home directory:
+
+`curl -sL "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"`
+curl -> a tool for transferring data from or to a server
+-s -> --silent (silent mode)
+-L -> --location (Follow redirects)
+
+Then install and run through the prompts:
+
+`bash  Miniconda3-latest-Linux-x86_64.sh`
+
+Finally restart your terminal to excecute changes (or run `source ~/.bashrc`)
+
+3. Create the conda environment
+
+Run `conda init`
+Run `make conda`
+
+4. Activate the conda environment
+
+`conda activate chess`
+
+### Installing and running the chess scraper
+
+1. Run `make packages` from command line.
+This command runs `pip install -e src/`
+
+The chess scraper is defined in the `src/` directory.
+
+2. Run the scraper
+
+`chess get-data -u {username} -f {output destination}`
--- src/scraper/client.py
+++ src/scraper/client.py
@@ -8,52 +8,62 @@ class ChessClient():
         pass
 
     def get_user(self, username) -> dict:
-        r = requests.get(f"https://api.chess.com/pub/player/{username}")
 
-        return r.json()
+        r = requests.get(f"https://api.chess.com/pub/player/{username}")
+        response = r.json()
+
+        if r.status_code == 200:
+            return response
+        
+        if r.status_code == 404:
+            print(response["message"])
+            return False
+
 
     def get_user_stats(self, username: str) -> dict:
+        if self.get_user(username):
 
-        r = requests.get(f"https://api.chess.com/pub/player/{username}/stats")
+            r = requests.get(f"https://api.chess.com/pub/player/{username}/stats")
 
-        return r.json()
+            return r.json()
 
     def get_user_archive_list(self, username: str) -> list:
+        if self.get_user(username):
 
-        r = requests.get(f"https://api.chess.com/pub/player/{username}/games/archives")
-        r = r.json()
+            r = requests.get(f"https://api.chess.com/pub/player/{username}/games/archives")
+            r = r.json()
 
-        print(f"Found {len(r['archives'])} archives")
+            print(f"Found {len(r['archives'])} archives")
 
-        return r['archives']
+            return r['archives']
 
-    def get_archive_data(self, username: str, year: int = None, month: int = None) -> pd.DataFrame:
+    def get_archive_data(self, username: str, file: str, year: int = None, month: int = None) -> pd.DataFrame:
+        if self.get_user(username):
+            processor = GameProcessor(username)
+            
+            if not year:
+                print(f"Collecting all games for {username}")
+                archives = self.get_user_archive_list(username)
+            
+                games = []
 
-        processor = GameProcessor(username)
-        
-        if not year:
-            print(f"Collecting all games for {username}")
-            archives = self.get_user_archive_list(username)
-        
-            games = []
+                for url in archives:
+                    r = requests.get(url)
+                    r = r.json()
 
-            for url in archives:
+                    for game in r["games"]:
+                        games.append(game)
+
+                print(f"Found {len(games)} games")
+
+                processor.archive_cleaner(pd.DataFrame(games)).to_json(file, orient="records")
+
+
+            else:
+                print(f"Collecting games for {username} from {month:02}/{year}")
+                url = f"https://api.chess.com/pub/player/{username}/games/{year}/{month:02}"
+
                 r = requests.get(url)
                 r = r.json()
 
-                for game in r["games"]:
-                    games.append(game)
-
-            print(f"Found {len(games)} games")
-
-            return processor.archive_cleaner(pd.DataFrame(games))
-
-
-        else:
-            print(f"Collecting games for {username} from {month:02}/{year}")
-            url = f"https://api.chess.com/pub/player/{username}/games/{year}/{month:02}"
-
-            r = requests.get(url)
-            r = r.json()
-
-            return processor.archive_cleaner(pd.DataFrame(r["games"]))
+                processor.archive_cleaner(pd.DataFrame(r["games"])).to_json(file, orient="records")
