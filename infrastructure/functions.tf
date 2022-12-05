5a6c3032f5f8740c13fab292225603e9dfabe218
stuart-watt <stuartwatt1996@gmail.com>
2022-12-05 07:11:58 +0000

    update memory and timeout of GCF

--- infrastructure/functions.tf
+++ infrastructure/functions.tf
@@ -22,8 +22,8 @@ resource "google_cloudfunctions_function" "chess_scraper" {
   source_archive_bucket = google_storage_bucket.artifacts.name
   source_archive_object = google_storage_bucket_object.function_archive.name
 
-  timeout               = 60
-  available_memory_mb   = 128
+  timeout               = 120
+  available_memory_mb   = 1024
 
   entry_point           = "main"
 
